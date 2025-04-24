b00be4d25bf96c4d9556b43c1efbe436c11a815d
Dejan <dejan_web@outlook.com>
2025-04-24 12:10:32 +0200

    busStationClassFinished

--- garageofVehicles.php
+++ garageofVehicles.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * A garage that has vehicles for transportation and has certain methods for easier work
  */
@@ -39,4 +40,44 @@ class Garage
         $this->vehicles[] = $vehicle;
         return $this;
     }
+}
+
+class BusStation
+{
+    protected array $buses;
+    /**
+     * bus route display methode
+     * @return void
+     */
+    public function displayRoutes()
+    {
+        echo "Avaliable bus routes: <br><hr>";
+        foreach ($this->buses as $bus) {
+            echo $bus->getRoute() . "<br><hr>";
+        }
+    }
+    /**
+     * a method that adds new buses to the array must have an implementation of the HashRoute interface
+     * @param HasRoute $bus
+     * @return static
+     */
+    public function addBus(HasRoute $bus)
+    {
+        $this->buses[] = $bus;
+        return $this;
+    }
+}
+
+interface HasRoute
+{
+    public function getRoute();
+}
+interface Vehicle
+{
+    public function display();
+    /**
+     * Summary of consumedFuel
+     * @return
+     */
+    public function consumedFuel();
 }
